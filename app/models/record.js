615a9bec27bc52256939fbc85dbd0b258a06c9e2
Brian Chan <b.chan88@gmail.com>
2017-09-20 16:52:01 -0700

    setup localstorage for ember data

--- app/models/record.js
+++ app/models/record.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  id: DS.attr('int'),
+  _id: DS.attr('number'),
   type: DS.attr('string'),
   time: DS.attr('date'),
   changes: DS.attr('string')
