d8b3704b96545ce6212c03de74782d78ccf8e4ee
Brian Chan <b.chan88@gmail.com>
2017-09-20 16:31:57 -0700

    able to read csv file

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,12 +2,26 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   inputCSV: null,
+  errorMessage: null,
 
   actions: {
-    uploadCSV(event) {
-      let inputCSV = this.get(inputCSV);
+    onchange(files) {
+      this.set('inputCSV', files)
+    },
 
-      console.log(event);
+    uploadCSV() {
+      let fileReader = new FileReader();
+
+      fileReader.readAsText(this.get('inputCSV')[0]);
+      fileReader.onload = (event) => {
+        console.log(event.target.result);
+      }
+
+      fileReader.onerror = (error) => {
+        if (error.target.error.name == 'NotReadableError') {
+          this.set('errorMessage', error.target.error.name);
+        }
+      }
     }
   }
 });
