ba2940cf3800439f3f89bec2305a9fb3a4a7a2a9
Brian Chan <b.chan88@gmail.com>
2017-09-20 20:48:46 -0700

    finish parse and save

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  store: Ember.inject.service(),
+
   inputCSV: null,
   errorMessage: null,
 
@@ -30,6 +32,20 @@ export default Ember.Controller.extend({
   },
 
   _parseCSV(csv) {
-    console.log(csv);
+    let rows = csv.split('\n');
+    rows.forEach((row) => {
+      let rowArray = row.split(',', 3);
+      let startOfChanges = row.indexOf('"');
+      let changesString = JSON.parse(row.substr(startOfChanges));
+
+      let record = this.get('store').createRecord('record', {
+        _id: parseInt(rowArray[0]),
+        type: rowArray[1],
+        timestamp: new Date(parseInt(rowArray[2]) * 1000),
+        changes: changesString
+      });
+
+      record.save();
+    });
   }
 });
--- app/models/record.js
+++ app/models/record.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   _id: DS.attr('number'),
   type: DS.attr('string'),
-  time: DS.attr('date'),
+  timestamp: DS.attr('date'),
   changes: DS.attr('string')
 });
