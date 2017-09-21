0b3fdce08dd451e5945211396266a002c02c4a5c
Brian Chan <b.chan88@gmail.com>
2017-09-20 22:38:52 -0700

    row component

---
+++ app/components/record-row/component.js
@@ -0,0 +1,38 @@
+import Ember from 'ember';
+
+const KEY_NAME_MAP = {
+  customer_name: 'Name',
+  customer_address: 'Address',
+  status: 'Status',
+  name: 'Name',
+  price: 'Price',
+  stock_levels: 'Stock Levels',
+  ship_date: 'Ship Date',
+  shipping_provider: 'Shipping Provider',
+  order_id: 'Order ID',
+  product_ids: 'Product IDs',
+  total: 'Total'
+}
+
+export default Ember.Component.extend({
+  tagName: 'tr',
+  record: null,
+
+  timestampString: Ember.computed('record.timestamp', function() {
+    return this.get('record.timestamp').toString();
+  }),
+
+  changes: Ember.computed('record.changes', function() {
+    let changesObject = JSON.parse(this.get('record.changes'));
+    let changesArray = Object.entries(changesObject);
+
+    changesArray.map((change) => {
+      let [key, value] = change;
+
+      return {
+        key: KEY_NAME_MAP[key],
+        value
+      };
+    })
+  })
+});
